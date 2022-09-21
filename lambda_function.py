cd9ca2c60f1182c9ef9c46055fc6e1c9d8f388b8
NiRovil <nicolas199851@hotmail.com>
2022-09-21 09:54:23 -0300

    Fixing lambda function.

--- lambda_function.py
+++ lambda_function.py
@@ -1,34 +1,60 @@
 from boto3 import Session
+from PIL import Image
 
-import pywhatkit
 import boto3
-import glob, os
+
+ACESS_KEY = ''
+SECRET_KEY = ''
+
+# acessa a sessão da aws
+session = Session(aws_access_key_id=ACESS_KEY, aws_secret_access_key=SECRET_KEY)
 
 def lambda_handler(event, context):
 
-    name = ''
-    directory = os.getcwd()
-
-    ACESS_KEY = ''
-    SECRET_KEY = ''
-
-    session = Session(aws_access_key_id=ACESS_KEY, aws_secret_access_key=SECRET_KEY)
-
+    # acessa os buckets
     s3 = session.resource('s3')
-    conversor_imagem = s3.Bucket('conversor-imagem')
+    bucket = s3.Bucket('conversor-imagem')
 
-    for s3_file in conversor_imagem.objects.all():
-        name = s3_file.key[:-4]
+    # busca todos os items dentro do bucket
+    s3_file_key = [s3_file.key for s3_file in bucket.objects.all()]
+    names = [s3_file.key[:-4] for s3_file in bucket.objects.all()]
 
-    s3 = boto3.client('s3')
+    for key in s3_file_key:
 
-    s3.download_file('conversor-imagem', s3_file.key, f'{directory}./imagem.png')
+        # busca o objeto e processa na memória, sem precisar de download
+        object = bucket.Object(key)
+        response = object.get()
+        file_stream = response['Body']
+        img = Image.open(file_stream).convert("L")
 
-    images = [file for file in glob.glob(f'{directory}/*.png')]
-    print(images)
+        # transforma a imagem em uma arte ascii
+        width, height = img.size
+        aspect_ratio = height / width
+        new_width = 80
+        new_height = aspect_ratio * new_width * 0.55
+        img = img.resize((new_width, int(new_height)))
 
-    for image in images:
-        txt_file = pywhatkit.image_to_ascii_art(image, name)
-        print(txt_file)
+        pixels = img.getdata()
 
-    s3.upload_file(name+'.txt', 'imagem-ascii', name+'.txt')
+        chars = ["*", "S", "#", "&", "@", "$", "%", "*", "!", ":", "."]
+        new_pixels = [chars[pixel // 25] for pixel in pixels]
+        new_pixels = "".join(new_pixels)
+
+        new_pixels_count = len(new_pixels)
+        ascii_image = [
+            new_pixels[index : index + new_width]
+            for index in range(0, new_pixels_count, new_width)
+        ]
+        ascii_image = "\n".join(ascii_image)
+
+        # atribui um nome ao arquivo
+        for name in names:
+            file_name = f'{name}.txt'
+
+        text = ascii_image.encode("utf-8")
+
+        # deleta a imagem após a conversão
+        s3.Object('conversor-imagem', key).delete()
+
+        # faz o upload do arquivo txt para o bucket 'imagem-ascii'
+        s3.Bucket('imagem-ascii').put_object(Key=f'{file_name}', Body=text)
